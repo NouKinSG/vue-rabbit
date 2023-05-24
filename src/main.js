e9bf74e6584a294a31e54b877a9871a7cbfef73d
NouKinSG <1768399321@qq.com>
2023-05-24 22:09:36 +0800

    axios基础封装

--- src/main.js
+++ src/main.js
@@ -6,6 +6,14 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+
+// 测试接口函数
+import { getCategory } from '@/apis/testapi' 
+
+getCategory().then(res=>{
+    console.log(res)
+})
+
 const app = createApp(App)
 
 app.use(createPinia())
