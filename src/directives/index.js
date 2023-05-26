fe0fde84cec2a1fd983871efb28999d7d33e3364
NouKinSG <1768399321@qq.com>
2023-05-26 21:06:04 +0800

    house-图片懒加载指令优化

---
+++ src/directives/index.js
@@ -0,0 +1,33 @@
+//定义懒加载插件
+// vueUse的导入
+import { useIntersectionObserver } from '@vueuse/core'
+
+export const lazyPlugin = {
+    install(app){
+        //懒加载指令逻辑
+
+        //定义全局指令
+        app.directive('img-lazy',{
+            mounted(el,binding){
+                // el：指令绑定的那个元素
+                // binding：bindding.value 指令等于号后面绑定的表达式的值  图片url
+               
+                // console.log(el,binding.value);
+
+                const {stop} = useIntersectionObserver(
+                    el,
+                    ([{ isIntersecting }],) => {
+                    // console.log(isIntersecting);
+                        if(isIntersecting){
+                            //元素进入视图区域
+                            el.src=binding.value
+                            stop()
+                        }
+                    },
+                )
+            }
+        })
+
+
+    }
+}
--- src/main.js
+++ src/main.js
@@ -9,15 +9,8 @@ import router from './router'
 //  引入初始化样式文件
 import '@/styles/common.scss'
 
-// 测试接口函数
-// import { getCategory } from '@/apis/testapi' 
-
-// getCategory().then(res=>{
-//     console.log(res)
-// })
-
-// vueUse的导入
-import { useIntersectionObserver } from '@vueuse/core'
+// 引入懒加载指令插件并且注册
+import {lazyPlugin}  from '@/directives'
 
 
 
@@ -26,26 +19,9 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(lazyPlugin)
 
 app.mount('#app')
 
 
-//定义全局指令
-app.directive('img-lazy',{
-    mounted(el,binding){
-        // el：指令绑定的那个元素
-        // binding：bindding.value 指令等于号后面绑定的表达式的值  图片url
-        console.log(el,binding.value);
 
-        useIntersectionObserver(
-            el,
-            ([{ isIntersecting }],) => {
-              console.log(isIntersecting);
-                if(isIntersecting){
-                    //元素进入视图区域
-                    el.src=binding.value
-                }
-            },
-          )
-    }
-})
