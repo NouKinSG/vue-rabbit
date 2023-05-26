379897d88bcb93fe734acc523ae270142e266457
NouKinSG <1768399321@qq.com>
2023-05-26 11:09:34 +0800

    home-新鲜好物业务实现

--- src/apis/home.js
+++ src/apis/home.js
@@ -6,6 +6,15 @@ export function getBannerAPI(){
     })
 }
 
-
+/**
+ * @description: 获取新鲜好物
+ * @param {*}
+ * @return {*}
+ */
+export const findNewAPI = () => {
+    return httpInstance({
+      url:'/home/new'
+    })
+  }
 
 
