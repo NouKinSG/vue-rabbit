02cb79ce2908a042eaf09c176a3b5fba6d94968e
NouKinSG <1768399321@qq.com>
2023-05-31 19:26:52 +0800

    二级分类-列表筛选功能的实现

--- src/views/SubCategory/index.vue
+++ src/views/SubCategory/index.vue
@@ -1,7 +1,9 @@
 <script setup>
 import { onMounted, ref } from 'vue';
-import {getCategoryFilterAPI} from '@/apis/category'
+import {getCategoryFilterAPI,getSubCategoryAPI} from '@/apis/category'
 import { useRoute } from 'vue-router';
+import GoodsItem from '../Home/components/GoodsItem.vue';
+
 
 //获取面包屑导航数据
 const categoryData =ref({})
@@ -16,6 +18,34 @@ const getCategoryDate = async ()=>{
 onMounted(()=>{
   getCategoryDate()
 })
+
+
+// 获取基础列表数据渲染
+const goodList = ref([])
+const reqData = ref({
+    categoryId:route.params.id,
+    page:1,
+    pagsize: 20,
+    sortField:'publishTime',
+})
+
+const getGoodList = async ()=>{
+  const res =await getSubCategoryAPI(reqData) 
+  // console.log(res);
+  goodList.value = res.result.items
+}
+
+onMounted(()=>{
+  getGoodList()
+})
+
+
+// tab切换回调
+const tabChange = ()=>{
+  console.log('tab切换了',reqData.value);
+  reqData.value.page=1
+  getGoodList()
+}
 
 
 
@@ -33,13 +63,15 @@ onMounted(()=>{
       </el-breadcrumb>
     </div>
     <div class="sub-container">
-      <el-tabs>
+      <el-tabs v-model="reqData.sortField" @tab-change="tabChange">
         <el-tab-pane label="最新商品" name="publishTime"></el-tab-pane>
         <el-tab-pane label="最高人气" name="orderNum"></el-tab-pane>
         <el-tab-pane label="评论最多" name="evaluateNum"></el-tab-pane>
       </el-tabs>
       <div class="body">
          <!-- 商品列表-->
+        <GoodsItem v-for="good in goodList" :good="good" :key="good.id"></GoodsItem>
+        
       </div>
     </div>
   </div>
