525ed0de66451deea8c76938ea5e29fd61c0d190
NouKinSG <1768399321@qq.com>
2023-06-01 11:58:38 +0800

    放大镜效果-滑块跟随鼠标移动

--- src/components/ImageView/index.vue
+++ src/components/ImageView/index.vue
@@ -1,5 +1,6 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, watch } from 'vue';
+import { useMouseInElement } from '@vueuse/core';
 
 // 图片列表
 const imageList = [
@@ -17,17 +18,45 @@ const enterhandler = (i)=>{
    activeIndex.value =i
 }
 
+// 2、获取鼠标相对位置（使用vueUse插件）
+const target = ref(null)
+const {elementX,elementY,isOutside} =useMouseInElement(target)
+
+// 3、控制滑块跟随鼠标移动（监听elementX/Y变化，一旦变化  重新设置left/top）
+const left = ref(0)
+const top = ref(0)
+  watch([elementX,elementY],()=>{
+    console.log('xy变化了');
+    //有效范围内控制滑块距离
+    //横向
+    if(elementX.value > 100 &&elementX.value<300){
+      left.value = elementX.value -  100
+    }
+    //纵向
+    if(elementY.value > 100 &&elementY.value<300){
+      top.value = elementY.value -  100
+    }
+
+    //处理边界
+    if(elementX.value > 300){ left.value=200 }
+    if(elementX.value < 100){  left.value=0  }
+
+    if(elementY.value > 300){ top.value=200 }
+    if(elementY.value < 100){  top.value=0  }
+
+  })
 
 </script>
 
 
 <template>
+  {{ elementX }},{{ elementY }},{{ isOutside }}
   <div class="goods-image">
     <!-- 左侧大图-->
     <div class="middle" ref="target">
       <img :src="imageList[activeIndex]" alt="" />
       <!-- 蒙层小滑块 -->
-      <div class="layer" :style="{ left: `0px`, top: `0px` }"></div>
+      <div class="layer" :style="{ left: `${left}px`, top: `${top}px` }"></div>
     </div>
     <!-- 小图列表 -->
     <ul class="small">
