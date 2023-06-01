d5e4e86da4ead97c57e1f76e53a25a94f6d05567
NouKinSG <1768399321@qq.com>
2023-06-01 12:11:41 +0800

    放大镜-实现大图显示以及优化

--- src/components/ImageView/index.vue
+++ src/components/ImageView/index.vue
@@ -25,8 +25,16 @@ const {elementX,elementY,isOutside} =useMouseInElement(target)
 // 3、控制滑块跟随鼠标移动（监听elementX/Y变化，一旦变化  重新设置left/top）
 const left = ref(0)
 const top = ref(0)
-  watch([elementX,elementY],()=>{
-    console.log('xy变化了');
+
+// 大图的相对坐标
+const positionX = ref(0)
+const positionY = ref(0)
+
+  watch([elementX,elementY,isOutside],()=>{
+    // console.log('xy变化了');
+    //如果鼠标没有移入到盒子里面  直接不执行后面的逻辑
+    if(isOutside.value)  return
+
     //有效范围内控制滑块距离
     //横向
     if(elementX.value > 100 &&elementX.value<300){
@@ -44,6 +52,9 @@ const top = ref(0)
     if(elementY.value > 300){ top.value=200 }
     if(elementY.value < 100){  top.value=0  }
 
+    // 控制大图的显示
+    positionX.value = -left.value *2
+    positionY.value = -top.value *2
   })
 
 </script>
@@ -56,7 +67,7 @@ const top = ref(0)
     <div class="middle" ref="target">
       <img :src="imageList[activeIndex]" alt="" />
       <!-- 蒙层小滑块 -->
-      <div class="layer" :style="{ left: `${left}px`, top: `${top}px` }"></div>
+      <div class="layer"  v-show="!isOutside"  :style="{ left: `${left}px`, top: `${top}px` }"></div>
     </div>
     <!-- 小图列表 -->
     <ul class="small">
@@ -68,10 +79,10 @@ const top = ref(0)
     <div class="large" :style="[
       {
         backgroundImage: `url(${imageList[0]})`,
-        backgroundPositionX: `0px`,
-        backgroundPositionY: `0px`,
+        backgroundPositionX: `${positionX}px`,
+        backgroundPositionY: `${positionY}px`,
       },
-    ]" v-show="false"></div>
+    ]" v-show="!isOutside"></div>
   </div>
 </template>
 
