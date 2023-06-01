3fd9606e94052852d664d6a264b0c552080e7305
NouKinSG <1768399321@qq.com>
2023-06-01 12:20:17 +0800

    图片预览组件-props适配以及优化操作

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -46,7 +46,7 @@ onMounted(()=>{
           <div class="goods-info">
             <div class="media">
               <!-- 图片预览区 -->
-              <ImageView></ImageView>
+              <ImageView :image-list="goods.mainPictures"></ImageView>
               <!-- 统计数量 -->
               <ul class="goods-sales">
                 <li>
